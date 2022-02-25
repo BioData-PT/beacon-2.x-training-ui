897254aeb13be464a33f079dbb2de2bf83342906
Marta Ferri Peradalta <marta.ferri@crg.eu>
2022-02-25 11:19:47 +0100

    Remove index view

--- app/beacon/urls.py
+++ app/beacon/urls.py
@@ -4,13 +4,13 @@ from . import views
 
 app_name = 'beacon'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('cohorts', views.cohorts, name='cohorts'),
+    path('', views.variant, name='variant'),
     path('variant', views.variant, name='variant'),
     path('variant_response', views.variant_response, name='variant_response'),
     path('region', views.region, name='region'),
     path('region_response', views.region_response, name='region_response'),
     path('phenoclinic', views.phenoclinic, name='phenoclinic'),
     path('phenoclinic_response', views.phenoclinic_response, name='phenoclinic_response'),
+    path('cohorts', views.cohorts, name='cohorts'),
     path('filtering_terms', views.filtering_terms, name='filtering_terms'),
 ]
