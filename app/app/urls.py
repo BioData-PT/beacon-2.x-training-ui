ea6e3026e4950d8c0423851e50949e66be81ba0b
FracassandoCasualmente <miguel.conrado.santos@tecnico.ulisboa.pt>
2023-01-31 18:38:09 +0000

    Beacon with debug = false but still can't work with statics

--- app/app/urls.py
+++ app/app/urls.py
@@ -22,3 +22,13 @@ urlpatterns = [
 ]
 
 handler404 = 'beacon.views.handle_page_not_found'
+
+# solution (inefficient) to static files not loading
+from django.conf.urls import url, re_path
+from django.conf import settings
+from django.views.static import serve
+
+urlpatterns += [
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
