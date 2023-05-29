5f68ae532ed099834dccf9bea4f4ffdf8cb00f06
FracassandoCasualmente <miguel.conrado.santos@tecnico.ulisboa.pt>
2023-05-29 15:33:53 +0100

    Increase log level

--- app/app/settings.py
+++ app/app/settings.py
@@ -172,7 +172,7 @@ LOGGING = {
     "loggers": {
         "django": {
             "handlers": ["console"],
-            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+            "level": os.getenv("DJANGO_LOG_LEVEL", "DEBUG"),
             "propagate": False,
         },
     },
