a2b3e0c0ada8ef4682932111e1d6e6e601f843b6
FracassandoCasualmente <miguel.conrado.santos@tecnico.ulisboa.pt>
2023-05-30 10:24:43 +0100

    Trying to fix path of middleware...

--- app/app/settings.py
+++ app/app/settings.py
@@ -77,7 +77,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'app.app.beacon.middleware.BeaconMiddleware' # custom middleware
+    'app.beacon.middleware.BeaconMiddleware' # custom middleware
 ]
 
 ROOT_URLCONF = 'app.urls'
